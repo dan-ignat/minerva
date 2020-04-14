dc2291cb94aa1ef6ae889b0e02c20772393b2dfb
Dan Ignat <5143789+dan-ignat@users.noreply.github.com>
2020-04-14 18:25:21 -0500

    Added TestMinervaWriter
    To document the decision not to unit test it

---
+++ src/test/java/name/ignat/minerva/TestMinervaWriter.java
@@ -0,0 +1,9 @@
+package name.ignat.minerva;
+
+/*
+ * Unlike MinervaReader, MinervaWriter is straightforward, without much branching, so it should be covered entirely by
+ * any of the Test*EndToEnd classes, so there's no need to duplicate coverage here.
+ */
+public class TestMinervaWriter
+{
+}
